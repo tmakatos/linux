a4a752b425672ffb3f57a5cc17cf84803d344bbc
Thanos Makatos <thanos.makatos@nutanix.com>
2022-05-25 08:34:01 -0400

    KVM: optionally commit write on ioeventfd write
    This patch is a slightly different take on the ioregionfd mechanism
    previously described here:
    https://lore.kernel.org/all/88ca79d2e378dcbfb3988b562ad2c16c4f929ac7.camel@gmail.com/
    
    The goal of this new mechanism, which we tentatively call shadow
    ioeventfd in lack of a better name, is to avoid an exit to the VMM when
    a particular GPA is written to. Without this mechanism, an exit to the
    VMM is necessary so that it communicates the event, along with the value
    written, to whoever is interested in this GPA.
    
    Our motivation behind this to speed up doorbell writes on NVMe
    controllers emulated outside of the VMM. Currently, a doorbell write to
    an NVMe SQ tail doorbell requires returning from ioctl(KVM_RUN) and the
    VMM communicating the event, along with the doorbell value, to the NVMe
    controller emulation task.  With the shadow ioeventfd, the NVMe
    emulation task is directly notified of the doorbell write and can find
    the doorbell value in a known location, without the interference of the
    VMM.
    
    RFC posted to KVM mailing list: https://marc.info/?l=kvm&m=166500585028727&w=2
    
    Set KVM_CAP_SHADOW_IOEVENTFD to a high numer (990) per ENG-532186 to
    avoid clashes in the foreseeable future.
    
    Signed-off-by: Thanos Makatos <thanos@nutanix.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -4702,6 +4702,7 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 	case KVM_CAP_IRQFD_RESAMPLE:
 	case KVM_CAP_MEMORY_FAULT_INFO:
 	case KVM_CAP_X86_GUEST_MODE:
+	case KVM_CAP_SHADOW_IOEVENTFD:
 		r = 1;
 		break;
 	case KVM_CAP_PRE_FAULT_MEMORY:
--- virt/kvm/eventfd.c
+++ virt/kvm/eventfd.c
@@ -693,6 +693,8 @@ struct _ioeventfd {
 	struct kvm_io_device dev;
 	u8                   bus_idx;
 	bool                 wildcard;
+	bool                 commit_write;
+	void                 *vaddr;
 };
 
 static inline struct _ioeventfd *
@@ -764,6 +766,10 @@ ioeventfd_write(struct kvm_vcpu *vcpu, struct kvm_io_device *this, gpa_t addr,
 	if (!ioeventfd_in_range(p, addr, len, val))
 		return -EOPNOTSUPP;
 
+	if (p->commit_write) {
+		if (unlikely(copy_to_user(p->vaddr, val, len)))
+			return -EFAULT;
+	}
 	eventfd_signal(p->eventfd);
 	return 0;
 }
@@ -842,6 +848,9 @@ static int kvm_assign_ioeventfd_idx(struct kvm *kvm,
 		p->datamatch = args->datamatch;
 	else
 		p->wildcard = true;
+
+	p->commit_write = args->flags & KVM_IOEVENTFD_FLAG_COMMIT_WRITE;
+	p->vaddr = (void *)args->vaddr;
 
 	mutex_lock(&kvm->slots_lock);
 
