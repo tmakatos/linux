bc0f155ae9744a046cb416e8c8fd5bc0f8916687
Thanos Makatos <thanos.makatos@nutanix.com>
2024-11-20 11:12:02 +0000

    ENG-717082: track number of outstanding IOs

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -214,6 +214,7 @@ struct nvme_queue {
 	__le32 *dbbuf_sq_ei;
 	__le32 *dbbuf_cq_ei;
 	struct completion delete_done;
+   atomic_t outstanding;
 };
 
 /*
@@ -471,6 +472,10 @@ static int nvme_pci_map_queues(struct blk_mq_tag_set *set)
  */
 static inline void nvme_write_sq_db(struct nvme_queue *nvmeq, bool write_sq)
 {
+    bool need_kick;
+    u16 submitted;
+    int new_outstanding;
+
 	if (!write_sq) {
 		u16 next_tail = nvmeq->sq_tail + 1;
 
@@ -480,10 +485,24 @@ static inline void nvme_write_sq_db(struct nvme_queue *nvmeq, bool write_sq)
 			return;
 	}
 
-	if (nvme_dbbuf_update_and_check_event(nvmeq->sq_tail,
-			nvmeq->dbbuf_sq_db, nvmeq->dbbuf_sq_ei))
-		writel(nvmeq->sq_tail, nvmeq->q_db);
+    if (nvmeq->sq_tail > nvmeq->last_sq_tail) {
+       submitted += nvmeq->sq_tail - nvmeq->last_sq_tail;
+    } else {
+       submitted += nvmeq->sq_tail + nvmeq->q_depth - nvmeq->last_sq_tail;
+    }
+    new_outstanding = atomic_add_return(submitted, &nvmeq->outstanding);
+    if (unlikely(new_outstanding > nvmeq->q_depth)) {
+        dev_warn(nvmeq->dev->ctrl.device,
+                "XXX qid=%hu submitted=%d outstanding=%d too many=%d (old_tail=%hu new_tail=%hu)\n",
+                nvmeq->qid, submitted, new_outstanding, new_outstanding > nvmeq->q_depth, nvmeq->last_sq_tail, nvmeq->sq_tail);
+    }
+
+    need_kick = nvme_dbbuf_update_and_check_event(nvmeq->sq_tail,
+			nvmeq->dbbuf_sq_db, nvmeq->dbbuf_sq_ei);
 	nvmeq->last_sq_tail = nvmeq->sq_tail;
+	if (need_kick) {
+    		writel(nvmeq->sq_tail, nvmeq->q_db);
+    }
 }
 
 static inline void nvme_sq_copy_cmd(struct nvme_queue *nvmeq,
@@ -1045,8 +1064,13 @@ static inline int nvme_process_cq(struct nvme_queue *nvmeq)
 		nvme_update_cq_head(nvmeq);
 	}
 
-	if (found)
+	if (found) {
+        int new_outstanding = atomic_sub_return(found, &nvmeq->outstanding);
+        if (unlikely(new_outstanding < 0)) {
+            dev_warn(nvmeq->dev->ctrl.device, "XXX qid=%hu completed=%d outstanding=%d too many=%d", nvmeq->qid, found, new_outstanding, new_outstanding < 0);
+        }
 		nvme_ring_cq_doorbell(nvmeq);
+    }
 	return found;
 }
 
@@ -1522,6 +1546,7 @@ static int nvme_alloc_queue(struct nvme_dev *dev, int qid, int depth)
 	nvmeq->cq_phase = 1;
 	nvmeq->q_db = &dev->dbs[qid * 2 * dev->db_stride];
 	nvmeq->qid = qid;
+    atomic_set(&nvmeq->outstanding, 0);
 	dev->ctrl.queue_count++;
 
 	return 0;
@@ -2240,7 +2265,7 @@ static int nvme_setup_io_queues(struct nvme_dev *dev)
 		nvme_suspend_io_queues(dev);
 		goto retry;
 	}
-	dev_info(dev->ctrl.device, "%d/%d/%d default/read/poll queues\n",
+	dev_info(dev->ctrl.device, "XXX thanos %d/%d/%d default/read/poll queues\n",
 					dev->io_queues[HCTX_TYPE_DEFAULT],
 					dev->io_queues[HCTX_TYPE_READ],
 					dev->io_queues[HCTX_TYPE_POLL]);
